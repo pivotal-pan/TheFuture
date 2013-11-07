fb61fb17b7d10bce70c49c995470b9c1ce84bc88
Don McCaughey and Rob Gallagher <pair+dmccaughey+rgallagher@pivotallabs.com>
2013-11-06 17:56:36 -0800

    Added ember data.

--- public/ember/js/app.js
+++ public/ember/js/app.js
@@ -1,28 +1,41 @@
 App = Ember.Application.create();
 
-App.Chart = Em.Object.extend();
-
-var charts = [];
-charts.push(App.Chart.create({id: 1}));
+App.Chart = DS.Model.extend({
+	progress: DS.attr('number')
+});
 
 App.Router.map(function() {
 	this.route('charts');
 	this.route('chart', { path: 'chart/:id' });
 });
 
+App.ApplicationRoute = Ember.Route.extend({
+	model: function() {
+		var store = this.get('store');
+		store.push('chart', {
+			id: 1,
+			progress: 0
+		});
+	},
+});
+
 App.ChartRoute = Ember.Route.extend({
 	model: function(params) {
 		return App.Chart.create(params);
+	},
+	activate: function() {
+		//
 	}
 });
 
 App.ChartsRoute = Ember.Route.extend({
 	model: function() {
-		return charts;
+		var store = this.get('store');
+		return store.all('chart');
 	},
 	actions: {
 		createChart: function(chartId) {
-			var chart = this.controller.pushObject(App.Chart.create({id: chartId}));
+			var chart = this.controller.pushObject(this.get('store').createRecord(App.Chart, {id: chartId}));
 			this.transitionTo('chart', chart);
 		}
 	}
